d5d6bffc8831bd88aa961d105e8c0d24d340c74c
rlloyd <rlloyd@federatedwireless.com>
2018-11-23 12:44:36 -0500

    add healthcheck

--- collect-80211.sh
+++ collect-80211.sh
@@ -2,13 +2,22 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+set -ex
+
+echo "checking required commands"
+which jq || exit 1
+which docker || exit 1
+which tshark || exit 1
+
+echo "waiting for elk to start..."
+elk_health=""
+until [ "${elk_health}" == '"healthy"' ]; do
+    elk_health=$(docker inspect elk | jq '. | .[0].State.Health.Status')
+    sleep 3
+done
+
 # download the filebeat container
 docker pull docker.elastic.co/beats/filebeat:6.2.4
-
-# uncomment the command below to setup dashboards
-#docker run --net elasticsearch_elknet -i \
-#        -v ${DIR}/filebeat/filebeat.yml:/usr/share/filebeat/filebeat.yml \
-#        docker.elastic.co/beats/filebeat:6.2.4 setup --dashboards
 
 # pass in -r <file> to specify a file or -i <interface>
 tshark -I -Tek $@ \
